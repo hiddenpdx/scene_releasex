36a97d9e2c5b012468850bda702d8d928803e295
hiddenpdx <hiddenparadox@penguinmail.com>
2025-12-26 21:40:38 +0100

    commit checksum

---
+++ checksum-Elixir.SceneReleasex.exs
@@ -0,0 +1,10 @@
+%{
+  "libscene_releasex_nif-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:fbf2a970823178457f14ab4cedc69d556e30114be8337473c268fa6be3cd1b4d",
+  "libscene_releasex_nif-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:ce78f29fbbe404718b98c037b5a64f31e978e8f1ef51ccce73c38d7e65553b7c",
+  "libscene_releasex_nif-v0.1.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:f1bf4322cb05667dd45ec516e8258fb2ba82aa0b0d9050eabf263689fc6843f9",
+  "libscene_releasex_nif-v0.1.0-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:78044e2fee8e87021de31e536f6cd352431e7c600cb4653575631ffd582c2878",
+  "libscene_releasex_nif-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:1fd47e32fd6bd5fb754779ed612bca3a4787f4e08577fdbf8c2bed6489cced36",
+  "libscene_releasex_nif-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:64c707848c5cf0f83c8099b68114448e3ef0bf8980d2d3fdc9e11bc419ba6352",
+  "scene_releasex_nif-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:96c28116590364300555c6f84eda79f66fc92a4518d709afda336a75f7c43b97",
+  "scene_releasex_nif-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:69e0fbe55d417142f3f0bac4ef0be9eb688c0ba325405a8f1c580d4f999eb3b8",
+}
